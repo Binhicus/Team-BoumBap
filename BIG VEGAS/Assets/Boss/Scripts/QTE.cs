c461c22bd08f3d1cd2f80b8b8ff9ed4fae2b0150
Antoine Letailleur <ant.letailleur@gmail.com>
2021-01-28 15:05:21 +0100

    QTE

---
+++ BIG VEGAS/Assets/Boss/Scripts/QTE.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QTE : MonoBehaviour
+{
+    public GameObject displayBox;
+    public GameObject passBox;
+    public int QTE_Gen;
+    public int waitingForKey;
+    public int correctKey;
+    public int countingDown;
+    public int score = 0;
+
+    void Update()
+    {
+        if(waitingForKey == 0)
+        {
+            QTE_Gen = Random.Range(1, 4);
+            countingDown = 1;
+            StartCoroutine(CountDown());
+
+            if(QTE_Gen == 1)
+            {
+                waitingForKey = 1;
+                displayBox.GetComponent<Text>().text = "[E]";
+            }
+
+            if (QTE_Gen == 2)
+            {
+                waitingForKey = 1;
+                displayBox.GetComponent<Text>().text = "[R]";
+            }
+
+            if (QTE_Gen == 3)
+            {
+                waitingForKey = 1;
+                displayBox.GetComponent<Text>().text = "[T]";
+            }
+        }
+
+        if(QTE_Gen == 1)
+        {
+            if(Input.anyKeyDown)
+            {
+                if(Input.GetButtonDown("EKey"))
+                {
+                    correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                    score++;
+                }
+                else
+                {
+                    correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+        if (QTE_Gen == 2)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("RKey"))
+                {
+                    correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                    score++;
+                }
+                else
+                {
+                    correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+        if (QTE_Gen == 3)
+        {
+            if (Input.anyKeyDown)
+            {
+                if (Input.GetButtonDown("TKey"))
+                {
+                    correctKey = 1;
+                    StartCoroutine(KeyPressing());
+                    score++;
+                }
+                else
+                {
+                    correctKey = 2;
+                    StartCoroutine(KeyPressing());
+                }
+            }
+        }
+
+        if(score == 20)
+        {
+            StartCoroutine(Win());
+        }
+    }
+
+    IEnumerator KeyPressing()
+    {
+        QTE_Gen = 4;
+        if(correctKey == 1)
+        {
+            countingDown = 2;
+            passBox.GetComponent<Text>().text = "FABULOUS!";
+            yield return new WaitForSeconds(0.75f);
+            correctKey = 0;
+            passBox.GetComponent<Text>().text = "";
+            displayBox.GetComponent<Text>().text = "";
+            yield return new WaitForSeconds(0.75f);
+            waitingForKey = 0;
+            countingDown = 1;
+        }
+
+        if (correctKey == 2)
+        {
+            countingDown = 2;
+            passBox.GetComponent<Text>().text = "FAIL...";
+            yield return new WaitForSeconds(2f);
+            correctKey = 0;
+            passBox.GetComponent<Text>().text = "LOSER!";
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(3.5f);
+        if(countingDown == 1)
+        {
+            QTE_Gen = 4;
+            countingDown = 2;
+            passBox.GetComponent<Text>().text = "FAIL...";
+            yield return new WaitForSeconds(1.5f);
+            correctKey = 0;
+            passBox.GetComponent<Text>().text = "";
+            displayBox.GetComponent<Text>().text = "";
+            yield return new WaitForSeconds(1.5f);
+            waitingForKey = 0;
+            countingDown = 1;
+            score--;
+        }
+    }
+
+    IEnumerator Win()
+    {
+        if(score == 20)
+        {
+            countingDown = 2;
+            yield return new WaitForSeconds(2f);
+            correctKey = 0;
+            passBox.GetComponent<Text>().text = "ROCKSTAR!";
+            score = 0;
+        }
+    }
+}
