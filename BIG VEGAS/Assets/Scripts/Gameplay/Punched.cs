1ce6314920ec765b5199934d1ac9af4a4765db90
Binhicus <abdoulaye-ba@live.fr>
2021-01-29 12:55:04 +0100

    enemy chases and punches

---
+++ BIG VEGAS/Assets/Scripts/Gameplay/Punched.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punched : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = GameManager.Instance.lastCheckPoint.position;
+        }
+    }
+}
--- BIG VEGAS/Assets/Scripts/Movement/PlayerInputActions.cs
+++ BIG VEGAS/Assets/Scripts/Movement/PlayerInputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Movement/PlayerInputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Movement/PlayerInputActions.inputactions'
 
 using System;
 using System.Collections;
