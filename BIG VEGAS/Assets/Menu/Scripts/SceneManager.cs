06ce8ee078e1ae35e84d6b3c8c1275b9945e2265
Antoine Letailleur <ant.letailleur@gmail.com>
2021-01-29 21:55:45 +0100

    Menu

---
+++ BIG VEGAS/Assets/Menu/Scripts/SceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public string mainScene;
+    public string credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // For restart a new party
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(mainScene);
+    }
+
+    public void  Credits()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(credits);
+    }
+
+    // To quit the game
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
