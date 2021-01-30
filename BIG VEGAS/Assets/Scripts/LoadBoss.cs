eec8f3c482093e1e71123d8951c45529caeb7ce1
Binhicus <abdoulaye-ba@live.fr>
2021-01-30 13:48:10 +0100

    pre_boss

---
+++ BIG VEGAS/Assets/Scripts/LoadBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBoss : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneBoss;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            BossFight();
+        }
+    }
+
+    void BossFight()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBoss);
+    }
+}
